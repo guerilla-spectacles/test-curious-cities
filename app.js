0d6c43bd067227ae3ebeef48c99beb4e12b27bf8
Ian Davis <icampbelldavis@gmail.com>
2015-04-20 15:00:41 -0700

    add uuid for unique url

--- app.js
+++ app.js
@@ -21,11 +21,12 @@ app.get('/', function(req, res){
 app.get('/sign_s3', function(req, res){
     aws.config.update({accessKeyId: AWS_ACCESS_KEY_ID , secretAccessKey: AWS_SECRET_ACCESS_KEY });
     var s3 = new aws.S3(); 
-    var unique_name = uuid.v4();
+    var unique_name = uuid.v1();
     var s3_params = { 
         Bucket: S3_BUCKET_NAME,
         // Name: unique_name,
-        Key: req.query.s3_object_name, 
+        // Key: req.query.s3_object_name,
+        Key: unique_name,
         Expires: 60, 
         ContentType: req.query.s3_object_type, 
         ACL: 'public-read'
@@ -37,7 +38,8 @@ app.get('/sign_s3', function(req, res){
         else{ 
             var return_data = {
                 signed_request: data,
-                url: 'https://'+S3_BUCKET_NAME+'.s3.amazonaws.com/'+req.query.s3_object_name 
+                url: 'https://'+S3_BUCKET_NAME+'.s3.amazonaws.com/'+s3_params.Key
+                // url: 'https://'+S3_BUCKET_NAME+'.s3.amazonaws.com/'+req.query.s3_object_name 
             };
             res.write(JSON.stringify(return_data));
             res.end();
