9afc0317498c5021bdd302219f4cec088a72b1be
Jahsie Ault <jahsieault@gmail.com>
2015-04-26 20:53:13 -0700

    More sidebars work.

--- public/javascripts/markersview.js
+++ public/javascripts/markersview.js
@@ -5,7 +5,7 @@
 var fakeDB = {};
 fakeDB.locations = [
 	{
-		title: '(Fake DB 1: Angst on Angst',
+		title: 'Fake DB 1: Angst on Angst',
 		description: 'This is a weird and ugly mural of some sort of hipster meta-angst.  #ugly',
 		curiousType: 'Art',
 		img: 'images/jollyRog.jpg',
@@ -15,7 +15,7 @@ fakeDB.locations = [
 	},
 	{
 		title: 'Fake DB 2: Portlandy picture',
-		description: 'Blah blah blah, picture picture kflasjf;lasd f;alskjdf falsdkj  #ugly',
+		description: 'Hey look! Its Portland!  #pdx',
 		curiousType: 'Stuff',
 		img: 'images/pdx.jpg',
 		latitude: 45.517555,
@@ -24,7 +24,7 @@ fakeDB.locations = [
 	},
 		{
 		title: 'Fake DB 3: Blurry Image',
-		description: 'Blah blah blah, picture picture kflasjf;lasd f;alskjdf falsdkj  #ugly',
+		description: 'This is a blurry mural. Not a good pucture #outoffocus',
 		curiousType: 'Stuff',
 		img: 'images/fontina.png',
 		latitude: 45.517255,
@@ -33,7 +33,7 @@ fakeDB.locations = [
 	},
 			{
 		title: 'Fake DB 4: Random placeholder',
-		description: 'Blah blah blah, picture picture kflasjf;lasd f;alskjdf falsdkj  #ugly',
+		description: 'Placeholder picture of the Portland trailblazers.#ripcity',
 		curiousType: 'Stuff',
 		img: 'images/blazers.jpg',
 		latitude: 45.51555,
@@ -104,58 +104,65 @@ var MarkerView = Backbone.View.extend({
 	// Render stationary middle flag
 	render: function(){
 		for (i=0; i < fakeDB.locations.length; i++) {
-		console.log(fakeDB.locations[i]);
-		console.log(fakeDB.locations[i].title);
-		// this.id = this.cid;
-		// console.log(this.id);
-		// console.log('"this is the marker view ID:' + this.cid);
-		var desc = fakeDB.locations[i].title;
-		// console.log(desc);
-		// console.log('hi!');
-		var latitude= fakeDB.locations[i].latitude;
-		var longitude = fakeDB.locations[i].longitude;
-		var img = fakeDB.locations[i].img;
-		// console.log('this is the image' + img);
-		marker = new google.maps.Marker({
+			// console.log(fakeDB.locations[i]);
+			// this.id = this.cid;
+			// console.log(this.id);
+			// console.log('"this is the marker view ID:' + this.cid);
+			var desc = fakeDB.locations[i].title;
+			// console.log(desc);
+			// console.log('hi!');
+			var latitude= fakeDB.locations[i].latitude;
+			var longitude = fakeDB.locations[i].longitude;
+			var img = fakeDB.locations[i].img;
+			// console.log('this is the image' + img);
+			marker = new google.maps.Marker({
 
-		//Google maps API stuff, helps prevent bugs.	
-		// if (this.model.get('description') =='undefined'){ description ='';} else { description = this.model.get('description');}
-		// if (this.model.telephone =='undefined'){ telephone ='';} else { telephone = this.model.telephone;}
-		// if (this.model.email =='undefined'){ email ='';} else { email = this.model.email;}
-		// if (this.model.web =='undefined'){ web ='';} else { web = this.model.web;}
-		// if (this.model.markerType =='undefined'){ icon ='images/map-marker-image.png';} else { icon = this.model.markerType;}      
-			
-			icon: 'images/map-marker-image.png',
-			position: new google.maps.LatLng(latitude, longitude),
-			map: map,
-			img: img,
-			title: fakeDB.locations[i].title,
-			description: fakeDB.locations[i].description,
-			category: fakeDB.locations[i].category,
-			id: 'markerLayer',
-		});
+			//Google maps API stuff, helps prevent bugs.	
+			// if (this.model.get('description') =='undefined'){ description ='';} else { description = this.model.get('description');}
+			// if (this.model.telephone =='undefined'){ telephone ='';} else { telephone = this.model.telephone;}
+			// if (this.model.email =='undefined'){ email ='';} else { email = this.model.email;}
+			// if (this.model.web =='undefined'){ web ='';} else { web = this.model.web;}
+			// if (this.model.markerType =='undefined'){ icon ='images/map-marker-image.png';} else { icon = this.model.markerType;}      
+				
+				icon: 'images/map-marker-image.png',
+				position: new google.maps.LatLng(latitude, longitude),
+				map: map,
+				img: img,
+				title: fakeDB.locations[i].title,
+				description: fakeDB.locations[i].description,
+				category: fakeDB.locations[i].category,
+				id: 'markerLayer',
+			});
+		
+			var infowindow = new google.maps.InfoWindow();
 
-		var infowindow = new google.maps.InfoWindow();
+			//Close any open infoWindow if the map is clicked (don't want more than one open at a time)
+			google.maps.event.addListener(map, 'click', function() {
+				infowindow.close();
+			});
 
-	//Close any open infoWindow if the map is clicked (don't want more than one open at a time)
-	google.maps.event.addListener(map, 'click', function() {
-		infowindow.close();
-	});
+			google.maps.event.addListener(marker, 'click', (function(marker, i) {
+				return function() {
 
-		google.maps.event.addListener(marker, 'click', (function(marker, i) {
-			return function() {
-				var infoWindowInfo = "<div id='guide-button-div' class='info-dropdown center'><h2>Closest Curiosties</h2><div class='curiousProfile'><h3>" + fakeDB.locations[i].title + "</h3><img class='curious-img' src=" + fakeDB.locations[i].img + "><p class='curious-description'>" + fakeDB.locations[i].description + "</p><p class='curious-type'>Category:" + fakeDB.locations[i].category + '</p></div>';
+					var infoWindowInfo = "<div id='guide-button-div' class='info-dropdown center'><h2>Closest Curiosties</h2><div class='curiousProfile'><h3>" + fakeDB.locations[i].title + "</h3><img class='curious-img' src=" + fakeDB.locations[i].img + "><p class='curious-description'>" + fakeDB.locations[i].description + "</p><p class='curious-type'>Category:<br>" + fakeDB.locations[i].category + '</p></div>';
 
-				//////////////Info Window if we want it ///////////////
-				// infowindow.setContent("<div id='guide-button-div' class='info-dropdown center'><h2>Closest Curiosties</h2><div class='curiousProfile'><h3>" + fakeDB.locations[i].title + "</h3><img class='curious-img' src=" + fakeDB.locations[i].img + "><p class='curious-description'>" + fakeDB.locations[i].description + "</p><p class='curious-type'>Category:" + fakeDB.locations[i].category + '</p></div>');
-				// infowindow.open(map, marker);
+					//////////////Info Window if we want it ///////////////
+					// infowindow.setContent("<div id='guide-button-div' class='info-dropdown center'><h2>Closest Curiosties</h2><div class='curiousProfile'><h3>" + fakeDB.locations[i].title + "</h3><img class='curious-img' src=" + fakeDB.locations[i].img + "><p class='curious-description'>" + fakeDB.locations[i].description + "</p><p class='curious-type'>Category:" + fakeDB.locations[i].category + '</p></div>');
+					// infowindow.open(map, marker);
 
-				var sidebar = document.getElementById('info-contents').innerHTML=infoWindowInfo;
-			}
+					////////MAkes the sidebar content from the marker info
+					document.getElementById('info-contents').innerHTML=infoWindowInfo;
+				}
 		})(marker, i));
-		locationList.push(marker);
-
+		locationList.push(marker.title);
 		};
+		console.log(locationList);
+		// for (i = 0; i <= locationList.length; i++) {
+		// 	console.log(locationList);
+		// 	console.log("location list" + locationList[i].title);
+		// };
+
+
 
 			// Makes markers clickable
 			// google.maps.event.addListener(marker, 'click', function() {
