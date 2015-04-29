110ffa9dd370177c158f99222d9c8a5beb5ed000
Ian Davis <icampbelldavis@gmail.com>
2015-04-28 20:42:32 -0700

    fix typo in index.js

--- routes/index.js
+++ routes/index.js
@@ -13,7 +13,6 @@ router.get('/', function(req, res) {
    db.newSearchBuilder()
     .collection(dbCollectionName)
     .limit(10)
-    .offset(offset)
     .query('*')
     .then(function (things){
       res.render('index');
