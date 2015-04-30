61c743c26c47d0cb4685f5751aed813019bffd84
Eric Gray <ericxgray@gmail.com>
2015-04-29 17:50:39 -0700

    Breaking up into three views

---
+++ public/javascripts/aboutView.js
@@ -0,0 +1,19 @@
+var AboutView = Backbone.View.extend({
+	el: '#info-contents',
+
+	initialize: function(){
+		var self = this;
+		//////////////////  About Curious Cities Sidebar//////////////////
+		about = "<div id=\"about-button-div\" class=\"info-dropdown center\">\
+			<h2>About Curious Cities</h2>\
+			<p>\
+				HI HI HI Magnis eros enim cursus. Et cursus adipiscing mauris ut aenean scelerisque tortor, facilisis adipiscing placerat scelerisque auctor platea, arcu in habitasse nunc pellentesque! Ut auctor et ut vel. In magna aliquet non! Ultrices sit sit nunc, enim turpis placerat ultricies dis lacus? Aliquet nec et in integer in ac elit placerat sed magna sagittis scelerisque!\
+			</p>\
+			<p>\
+				Sed! Urna! Egestas turpis hac enim nascetur arcu integer, risus augue sit ultrices facilisis egestas, massa amet, porttitor montes nisi, pulvinar est vut porttitor enim? Tincidunt? Mus, sed platea a etiam eu aliquam ut enim. Etiam risus porttitor adipiscing velit mus, urna dapibus, nec! Natoque placerat, urna, non in magna, nisi elit odio! Vel risus nunc urna mauris urna facilisis et urna facilisis rhoncus integer.\
+			</p>\
+		</div>";
+
+		$('#info-contents').append(about);		
+	}
+})
---
+++ public/javascripts/oddView.js
@@ -0,0 +1,161 @@
+var OddView = Backbone.View.extend({
+	el: '#info-contents',
+	events: {
+		'click #save' : 'save',
+		'change #location-file-input': 'extractLocation',
+	},
+
+	initialize: function(){
+		var self = this;
+		//Template for marker info
+		addLoc = "<div id=\"add-button-div\" class=\"info-dropdown center\">\
+			<h2>Add a new location</h2>\
+			<form>\
+				<input type='text' id='locationName' required name='location-name' placeholder='Location name'>\
+				<br>\
+				<textarea id='description' name='location-description' maxlength='140' placeholder='Description'></textarea>\
+				<br>\
+				<select id='catType' name='oddity-type'>\
+				<option value='Unspecified'>Category</option>\
+				<option value='Art'>Art</option>\
+				<option value='Nature'>Nature</option>\
+				<option value='Architecture'>Architecture</option>\
+				<option value='Other'>Other</option>\
+				</select>\
+				<p>Upload an image of the location!<br>\
+				</p>\
+				<!--\
+				HOPING TO MAKE THIS DECORATIVE\
+				<div class='fileUpload' required>\
+    				<span>Upload</span>\
+    				<input placeholder='choose file' id='selectedFile' type='file' class='form-button'>\
+				</div>\
+				-->\
+				<span>Upload file to see location in console</span>\
+				<input id=\"location-file-input\" type=\"file\" accept=\"image/jpeg\" />\
+				<p id=\"status\"></p>\
+				<p id=\"locationURL\"></p>\
+				<p class='always-hidden' id=\"latitideP\"></p>\
+				<p class='always-hidden' id=\"longitudeP\"></p>\
+				<div id=\"preview\"><img src=\"images/uploadPlaceholder.png\" style=\"width:300px;\" /></div>\
+				<br>\
+				<input type='submit' id='save' value='Submit'>\
+			</form>\
+		</div>";
+		$('#info-contents').append(addLoc);
+	},
+
+	//////////////////  Controls the sumbit/save button event //////////////////
+	save: function(event){
+		// event.preventDefault();
+		var $locationName = $(this.el).find('#locationName').val();
+		var $locationDesc = $(this.el).find('#description').val();
+		var $categoryType = $(this.el).find("#selected").val();
+		var $locationURL = $(this.el).find("#locationURL").html();
+		var $locationLatitude = $(this.el).find("#latitideP").html();
+		var $locationLongitude = $(this.el).find("#longitudeP").html();
+		var newMarker = {
+				img: ($locationURL),
+				title: ($locationName),
+				description: ($locationDesc ),
+				category: ($categoryType),
+				latitude: ($locationLatitude),
+				longitude: ($locationLongitude)
+				}
+
+
+		toMap = function() {
+
+
+			mapLocs.create(newMarker);
+
+		};
+		toMap();
+	},
+
+	extractLocation: function(e) {
+	var photoFile = e.target.files[0];
+	// if more than 1 file has been select, or if the selected file in bigger than 4MB
+	if ((e.target.files.length !== 1) || (photoFile.size > 4000000)){
+		alert("Please select a single jpeg file that is less that 4MB");
+	} else {
+		EXIF.getData(photoFile, function() {
+	    	var photoData = EXIF.getAllTags(this);
+	    	console.log(this);
+	    	console.log(photoData);
+	        if (photoData.GPSLatitudeRef != null || photoData.GPSLatitudeRef != undefined){
+	        	var location = {},
+	        		degreesLat = photoData.GPSLatitude[0], 
+	        		minutesLat = photoData.GPSLatitude[1], 
+	        		secondsLat = photoData.GPSLatitude[2],
+	        		degreesLong = photoData.GPSLongitude[0], 
+	        		minutesLong = photoData.GPSLongitude[1], 
+	        		secondsLong = photoData.GPSLongitude[2],
+	        		latitude = degreesLat + (minutesLat / 60) + (secondsLat / 3600),
+	        		longitude = degreesLong + (minutesLong / 60) + (secondsLong / 3600);
+	        		
+        		if (photoData.GPSLatitudeRef === "S") {
+        			latitude = ("-" + latitude);
+        		} 
+        		if (photoData.GPSLongitudeRef === "W") {
+        			longitude = ("-" + longitude);
+        		}
+
+        		location.latitude = parseFloat(latitude);
+
+        		location.longitude = parseFloat(longitude);
+
+		        //degrees is photoData.GPSLatitude[0], minutes is photoData.GPSLatitude[1], photoData.GPSLatitude[2]
+		        // Decimal Degrees = Degrees + minutes/60 + seconds/3600
+		        ////////s#_upload ////////////////
+		        var locURL = document.getElementById("locationURL");
+		        var locLatitude = document.getElementById("latitideP");
+		        var locLongitude = document.getElementById("longitudeP");
+		        var status_elem = document.getElementById("status");
+			    // var url_elem = document.getElementById("picture_url");
+			    var preview_elem = document.getElementById("preview");
+			    var s3upload = new S3Upload({
+			        file_dom_selector: 'location-file-input',
+			        s3_sign_put_url: '/sign_s3',
+			        onProgress: function(percent, message) {
+			            status_elem.innerHTML = 'Upload progress: ' + percent + '% ' + message;
+			        },
+			        onFinishS3Put: function(public_url) {
+			            status_elem.innerHTML = 'Upload completed.';
+			            // url_elem.value = public_url;
+			            preview_elem.innerHTML = '<img src="'+public_url+'" style="width:300px;" />';
+			            //console.log(public_url);
+			            var publicURL = public_url;
+			            $(this.el).find("#status").val(public_url);
+			            var $statusID = $(this.el).find("#status");
+			            console.log($statusID);
+			            locationURL.innerHTML = public_url;
+			            locLatitude.innerHTML = location.latitude;
+			            locLongitude.innerHTML = location.longitude;
+			            // console.log(public_url);
+			            // console.log('here we go');
+			            // console.log(publicURL);
+			            //$(this.el).find("#status").val(public_url); //add public url to html id
+			        },
+			        onError: function(status) {
+			            status_elem.innerHTML = 'Upload error: ' + status;
+			        }
+		    	});
+
+		    	//$locationURL.val('changed location value');
+		    	//console.log($locationURL).val();
+		        // return location; 
+		        //console.log(location);
+
+		        // console.log(photoData);
+			    } 
+		    	else {
+		    		alert("file contains no location data");
+		    	}
+	        }
+		)}
+
+	}
+
+
+})
